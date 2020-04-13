59d0fcfc484c80bf454c3cd8368265b97be58568
Yamel Sarabia <sarabia.yamel@gmail.com>
2020-04-13 00:40:37 -0700

    Comment big footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,105 +1,48 @@
-<div class="footer-wrapper">
-		<div class="footer-inner-wrapper">
-		<!-- Get Footer Widget -->
-			<div class="container footer-container">
-				<div class="footer-widget-wrapper">
-					<div class="row">
-						<div class="col-sm reviews-container">
-				      		<div class="elfsight-app-0c56033b-3cc3-4a3d-8c66-b3f3d4a0148f"></div>
-					    </div>
-					    <div class="col-sm">
-					      	<div class="row">
-					      		<div class="col-sm">
-						      		Contacto:
-						      		<div class="row">
-							      		<div class="col-sm">
-								      		Cel: (045) 7441-20-6091
-									    </div>
-									</div>
-									Horario:
-						      		<div class="row">
-							      		<div class="col-sm">
-								      		Nos encuentras los 365 días del año en Avenida Fuerza Aerea Mexicana, Barra de Coyuca de Benitez, Guerrero
-									    </div>
-									</div>
-							    </div>
-					      		<div class="col-sm">
-						      		MAPS
-						      		<!--The div element for the map -->
-										    <div class="mapouter"><div class="gmap_canvas"><iframe width="345" height="420" id="gmap_canvas" src="https://maps.google.com/maps?q=enramada%20el%20canalla%20barra%20de%20coyuca&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net/embed-google-map-in-website/">embedgooglemap.net</a></div><style>.mapouter{position:relative;text-align:right;height:420px;width:345px;}.gmap_canvas {overflow:hidden;background:none!important;height:420px;width:345px;}</style></div>
-									    
-							    </div>
-							</div>
-					    </div>
-<!--The div element for the map -->
-    <div id="map"></div>
-
-
-						<!--div class="six columns gdl-footer-1 mb0">
-									<!--iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FRogelioFloresBailon%2Fposts%2F1164388480287566%3A0&width=500" width="500" height="335" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
-									<>
-									<div class="elfsight-app-0c56033b-3cc3-4a3d-8c66-b3f3d4a0148f"></div>
-								
+<!--div class="footer-wrapper">
+	<div class="footer-inner-wrapper">
+		<div class="container footer-container">
+			<div class="footer-widget-wrapper">
+				<div class="row">
+					<div class="col-sm reviews-container">
+			      		<div class="elfsight-app-0c56033b-3cc3-4a3d-8c66-b3f3d4a0148f"></div>
+				    </div>
+				    <div class="col-sm">
+				      	<div class="row">
+				      		<div class="col-sm">
+					      		Contacto:
+					      		<div class="row">
+						      		<div class="col-sm">
+							      		Cel: (045) 7441-20-6091
+								    </div>
+								</div>
+								Horario:
+					      		<div class="row">
+						      		<div class="col-sm">
+							      		Nos encuentras los 365 días del año en Avenida Fuerza Aerea Mexicana, Barra de Coyuca de Benitez, Guerrero
+								    </div>
+								</div>
+						    </div>
+				      		<div class="col-sm">
+					      		MAPS
+								<div class="mapouter"><div class="gmap_canvas"><iframe width="345" height="420" id="gmap_canvas" src="https://maps.google.com/maps?q=enramada%20el%20canalla%20barra%20de%20coyuca&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net/embed-google-map-in-website/">embedgooglemap.net</a></div><style>.mapouter{position:relative;text-align:right;height:420px;width:345px;}.gmap_canvas {overflow:hidden;background:none!important;height:420px;width:345px;}</style></div>
+						    </div>
 						</div>
-						<div class="three columns gdl-footer-2 mb0">		
-							<div class="custom-sidebar">		
-								<h3 class="custom-sidebar-title">Contacto</h3>		
-								<ul>
-									<li>
-										<a href="https://demo.goodlayers.com/delicieux/this-is-just-a-single-clean-post/">This is Just a Single Clean Post</a>
-									</li>
-									<li>
-										<a href="https://demo.goodlayers.com/delicieux/etiam-porta-sem/">Post With Left Sidebar</a>
-									</li>
-									<li>
-										<a href="https://demo.goodlayers.com/delicieux/aenean-lacinia-bibendum/">Post With Slider Thumbnail</a>
-									</li>
-									<li>
-										<a href="https://demo.goodlayers.com/delicieux/adipiscing-fringilla-commodo/">Post Different Thumbnail</a>
-									</li>
-									<li>
-										<a href="https://demo.goodlayers.com/delicieux/ullamcorper-cras-aenean/">Post Full Width</a>
-									</li>
-									<li>
-										<a href="https://demo.goodlayers.com/delicieux/purus-inceptos-euismod-sit/">Purus Inceptos Euismod Sit</a>
-									</li>
-									<li>
-										<a href="https://demo.goodlayers.com/delicieux/fringilla-commodo-aenean/">Fringilla Commodo Aenean</a>
-									</li>
-								</ul>
-							</div>
-						</div>
-						<div class="three columns gdl-footer-3 mb0">
-							<div class="custom-sidebar">
-								<h3 class="custom-sidebar-title">Ubicación y Horario:</h3>		
-								<ul>
-									<li><a href="https://demo.goodlayers.com/delicieux/2012/06/">June 2012</a></li>
-									<li><a href="https://demo.goodlayers.com/delicieux/2011/12/">December 2011</a></li>
-									<li><a href="https://demo.goodlayers.com/delicieux/2011/11/">November 2011</a></li>
-									<li><a href="https://demo.goodlayers.com/delicieux/2011/09/">September 2011</a></li>
-									<li><a href="https://demo.goodlayers.com/delicieux/2011/08/">August 2011</a></li>
-									<li><a href="https://demo.goodlayers.com/delicieux/2011/07/">July 2011</a></li>
-									<li><a href="https://demo.goodlayers.com/delicieux/2010/09/">September 2010</a></li>
-								</ul>
-							</div>
-						</div-->						
-						<div class="clear"></div>
-					</div> <!-- close row -->
+				    </div>					
+					<div class="clear"></div>
 				</div>
-			</div> 
-		</div><!-- footer innerwrapper -->
-	</div>
-
-	<div class="copyright-outer-wrapper">
-		<div class="copyright-wrapper">
-			<div class="container copyright-container">
-				<div class="copyright-left">
-					Reserva ahora : (045) 7441-20-6091 &nbsp; &nbsp;					
-				</div> 
-				<div class="copyright-right">
-					© Copyright 2020 Yamel Sarabia
-				</div> 
-				<div class="clear"></div>
 			</div>
+		</div> 
+	</div>
+</div-->
+<div class="copyright-outer-wrapper">
+	<div class="copyright-wrapper">
+		<div class="container copyright-container">
+			<div class="copyright-left">
+				Reserva ahora : (045) 7441-20-6091					
+			</div> 
+			<div class="copyright-right">
+				© Copyright 2020 Yamel Sarabia
+			</div> 
 		</div>
+	</div>
 </div>
