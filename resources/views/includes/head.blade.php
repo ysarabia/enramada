c259a461f869e7a2ae2956c0084feb588db1e4fb
Yamel Sarabia <sarabia.yamel@gmail.com>
2020-07-08 21:30:33 -0700

    Add flag icons for language

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8">
 <meta name="description" content="deliciosa comida">
 <meta name="enramada" content="blade">
-<title>{{ config('app.name', 'Laravel') }}</title>
+<title>{{ config('app.name', 'El Canalla') }}</title>
 <!-- load bootstrap from a cdn -->
--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -3,21 +3,21 @@
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-   @php $locale = session()->get('locale'); @endphp
+  @php $locale = session()->get('locale'); @endphp
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
       <a class="nav-item nav-link navbar-toggle-pair" href="#">@lang('messages.menu.home')</a>
       <a class="nav-item nav-link navbar-toggle-spare" href="#">@lang('messages.menu.about_us')</a>
       <a class="nav-item nav-link navbar-toggle-pair" href="#">@lang('messages.menu.menu')</a>
       <a class="nav-item nav-link navbar-toggle-spare" href="#">@lang('messages.menu.booking')</a>
-      <a class="nav-item nav-link navbar-toggle-spare" href="#">@lang('messages.menu.gallery')</a>
+      <a class="nav-item nav-link navbar-toggle-pair" href="#">@lang('messages.menu.gallery')</a>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle navbar-toggle-spare" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           {{strtoupper($locale)}}
         </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="lang/es">ES</a>
-          <a class="dropdown-item" href="lang/en">EN</a>
+        <div class="dropdown-menu toggle-options" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="lang/es"><span>ES</span> <img style="padding-left: 25px;vertical-align: middle;" src={{ ('assets/images/mexico-flag-icon-32.png') }}></a>
+          <a class="dropdown-item" href="lang/en"><span>EN</span> <img style="padding-left: 25px;vertical-align: middle;" src={{ ('assets/images/canada-flag-icon-32.png') }}></a>
         </div>
       </li>
     </div>
